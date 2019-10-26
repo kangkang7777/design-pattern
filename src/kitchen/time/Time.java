afbc4c8092ac70d0c1a3aeeb55c455b214427567
Kriskr <37978646+HuangDuLiGong1999@users.noreply.github.com>
2019-10-26 16:29:20 +0800

    :tada: time

---
+++ src/kitchen/time/Time.java
@@ -0,0 +1,61 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Time {
+
+    /**
+     *
+     */
+    private Time() {
+    }
+
+    /**
+     * 
+     */
+    private static Time instance;
+
+    /**
+     * 
+     */
+    private ArrayList<TimeObserver> timeObservers;
+
+
+    /**
+     * @return
+     */
+    public static synchronized Time getInstance() {
+        if (instance == null) instance = new Time();
+
+        return instance;
+    }
+
+    /**
+     * 
+     */
+    public void update(int ticks) {
+        while(ticks>0)
+        {
+            for (TimeObserver o : timeObservers)  o.update();
+            ticks--;
+        }
+
+    }
+
+    /**
+     * @param o
+     */
+    public void addTimeObserver(TimeObserver o) {
+        timeObservers.add(o);
+    }
+
+    /**
+     * @param o
+     */
+    public void removeTimeObserver(TimeObserver o) {
+        timeObservers.remove(o);
+    }
+
+}
---
+++ src/kitchen/time/TimeObserver.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public interface TimeObserver {
+
+
+    /**
+     * 
+     */
+    void update();
+
+}
