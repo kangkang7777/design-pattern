b71b453c4f5fe176da27ee191bd99094d7a83aca
SongLiang <1173646513@qq.com>
2019-11-03 22:52:49 +0800

    修改MerchNameFactory  和文档MerchName包

--- src/kitchen/order/merchname/MerchNameFactory.java
+++ src/kitchen/order/merchname/MerchNameFactory.java
@@ -5,23 +5,44 @@ public class MerchNameFactory {
     static public List<String> getMerchName(String name){
         MerchName merchName=null;
         switch(name){
+            case "1":
+            merchName=new BoiledFish();
+            break;
             case "水煮鱼":
             merchName=new BoiledFish();
+            break;
+            case "2":
+            merchName=new EggSoup();
             break;
             case "鸡蛋汤":
             merchName=new EggSoup();
             break;
+            case "3":
+            merchName=new MaboTofu();
+            break;
             case "麻婆豆腐":
             merchName=new MaboTofu();
+            break;
+            case "4":
+            merchName=new StirFriedVegetables();
             break;
             case "炒青菜":
             merchName=new StirFriedVegetables();
             break;
+            case "5":
+            merchName=new SteamedBread();
+            break;
             case "馒头":
             merchName=new SteamedBread();
             break;
+            case "6":
+            merchName=new BoiledFishSet();
+            break;
             case "水煮鱼套餐":
             merchName=new BoiledFishSet();
+            break;
+            case "7":
+            merchName=new MaboTofuSet();
             break;
             case "麻婆豆腐套餐":
             merchName=new MaboTofuSet();
