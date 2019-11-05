1c3db01c3d2904592f71f27d51e1eb7ba0f46f2d
Kriskr <37978646+HuangDuLiGong1999@users.noreply.github.com>
2019-11-05 14:38:03 +0800

    add Ingredient Iterator descriptions

--- src/kitchen/container/ContainerIterator.java
+++ src/kitchen/container/ContainerIterator.java
@@ -5,39 +5,25 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- * 
+ * 容器迭代器类
+ * 设计模式：迭代器模式
+ * @author Vector Xie
  */
 public class ContainerIterator implements Iterator<Ingredient>{
 
-
-    /**
-     * 
-     */
     private ArrayList<Ingredient> ingredients;
 
-    /**
-     * 
-     */
     private int index;
 
-    /**
-     * @param ingredients
-     */
     public ContainerIterator(ArrayList<Ingredient> ingredients) {
        this.ingredients = ingredients;
        index = 0;
     }
 
-    /**
-     * @return
-     */
     public boolean hasNext() {
         return index < ingredients.size();
     }
 
-    /**
-     * @return
-     */
     public Ingredient next() {
         return ingredients.get(index++);
     }
