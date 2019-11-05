7450e3956274dda203a153d1c067397122a14773
Kriskr <37978646+HuangDuLiGong1999@users.noreply.github.com>
2019-11-05 14:19:35 +0800

    add time description

--- src/kitchen/time/TimeObserver.java
+++ src/kitchen/time/TimeObserver.java
@@ -3,14 +3,12 @@ package kitchen.time;
 import java.util.*;
 
 /**
- * 
+ * 时间观察者接口
+ * @description 控制原料新鲜成都
+ * @author Vector Xie
  */
 public interface TimeObserver {
 
-
-    /**
-     * 
-     */
     void update();
 
 }
